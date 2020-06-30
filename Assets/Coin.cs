767c2dcfc790627f8d118692f62fcbf2f294558d
OmarMendozaE33 <omarmendozae33@hotmail.com>
2020-06-29 21:06:42 -0500

    Añadi el script de coin y de move

---
+++ Assets/Coin.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    public float rotationSpeed;
+
+    private void Update()
+    {
+
+        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles + new Vector3(0f, rotationSpeed * Time.deltaTime, 0f));
+
+    }
+}
