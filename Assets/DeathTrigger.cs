f5aea0ab08e3931473e809a992131eb04d3dc4b7
iazmoon <ijazmin98@hotmail.com>
2020-06-29 22:46:26 -0500

    Agregue el deathtrigger y si se toca se reincia el lvl

---
+++ Assets/DeathTrigger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class DeathTrigger : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+
+    }
+
+
+}
