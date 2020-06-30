90733b2d90355f6ef93d02609411d5ad73de1bdd
imBushido <maxsakamoto123@gmail.com>
2020-06-29 21:30:38 -0500

    Cree Coin y Player tambien le añadi un script a coin para que desaparezca.

---
+++ Assets/Coin.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    public float rotationSpeed;
+  
+
+    private void Update()
+    {
+
+        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles + new Vector3(0f, rotationSpeed * Time.deltaTime, 0f));
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(this.gameObject);
+        
+    }
+}
